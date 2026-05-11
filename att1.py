0cc392c96e89a4ee95a40d7a2fced0d2a13e9a82
Bernardo Carvalho <bernardocarvalhopena@gmail.com>
2026-05-11 09:43:03 -0300

    Update att1.py

--- att1.py
+++ att1.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
+app = Flask(__name__)
 
-app = Flask(__name__) # inicio o flask
 
-@app.route('/') # Isso é o decorator, ele é usado para mapear a função abaixo para a rota '/'
+@app.route("/decorator")
 def decoratorex():
-    return 'Um decorator em Python é uma ferramenta poderosa que permite alterar ou estender o comportamento de uma função, método ou classe sem precisar modificar o seu código-fonte original!' # Isso é o que será retornado quando a rota '/' for acessada
+    return "Welcome to Flask’s documentation. Flask is a lightweight WSGI web application framework. It is designed to make getting started quick and easy, with the ability to scale up to complex applications."
 
-if __name__ == '__main__':
-    app.run(debug=True) # Isso inicia o servidor Flask em modo de depuração, o que é útil para desenvolvimento
+
+if __name__ == "__main__":
+    app.run(debug=True)
